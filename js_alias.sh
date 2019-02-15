031bfaa05d5e79bdfc1df0cfbcacf6cb1d3a33c0
johnsonlees <47136919+johnsonlees@users.noreply.github.com>
2019-02-15 15:32:07 +0800

    Create js_alias.sh

---
+++ js_alias.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+alias rdt='rdesktop -g1362x746 -0 -a24 '
+alias rto='rdt -rclipboard:PRIMARYCLIPBOARD -rsound:local '
+alias rtoadm='rto -uadministrator '
+alias rtodsk='rto -rdisk:HomeDsk=/home -rdisk:AllDsk=/media '
+
+alias r5572='rtoadm    -pHuidutech@2017 192.168.55.72  & '
+alias r1797='rtoadm    -phhxxttxs@HDF2  192.168.17.97  & '
+alias r101=' rtoadm    -p123321qweewq   192.168.30.101 & '
+alias r36='  rtoadm    -p123321qweewq36 192.168.30.36 & '
+alias r37='  rtoadm    -p123321qweewq37 192.168.30.37 & '
+alias r50='  rtoadm    -pHuidutech@2018 192.168.30.50 & '
+alias r124=' rtodsk    -uadministrator -pHuidutech@2018 192.168.21.124  & '
+alias r230=' rto -uhd  -p123321qweewq   192.168.30.230 & '
+alias rh230='rto -uhd  -p123321qweewq   112.3.57.99:30230 & '
+
+alias ansir=' sudo ansible '
+alias updt=' sudo ansible-pull -U https://github.com/johnsonlees/asbp updt.yml '
+
+alias cdf=' cd /home/lee/self/frequently '
+
+alias grepip=' grep -e "[[:digit:]]\{1,3\}\.[[:digit:]]\{1,3\}\.[[:digit:]]\{1,3\}\.[[:digit:]]\{1,3\}" '
+alias ms156=' mysql -ursyslog -pHuidusec@2019 -h192.168.55.156 -DSyslog '
+alias r=' reset '
+alias x=' startx & '
+alias vdir='vdir --color=auto'
