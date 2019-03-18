05f1966270272bb14453fd070bf3304179e9ea9f
hd <hd@localhost.localdomain>
2019-03-18 09:08:16 +0800

    mdf files

--- glrpcsvbr/asb/startglr.sh
+++ glrpcsvbr/asb/startglr.sh
@@ -1,34 +1,73 @@
 #!/bin/bash
 case "$1" in
   start)
-        /usr/bin/echo " START galera_new_cluster IN glr `date` "  ; 
         /usr/bin/sleep 30
-        /usr/bin/galera_new_cluster  
-        tt=$[$RANDOM % 77 + 37] 
-        /usr/bin/echo " START sleep and wakeup IN glr $tt `date` "  ; 
-        /usr/bin/sleep $tt 
-        /usr/bin/echo " START mariadb.service IN glr `date` " ;
-        /usr/bin/systemctl start mariadb  
+
+        if [[ $( cat /var/lib/mysql/grastate.dat | grep 'safe_to_bootstrap: 1' ) = 'safe_to_bootstrap: 1' ]]; then 
+            /usr/bin/echo "--jsmk-- CFGFILE safe_to_bootstrap: 1 IN /var/lib/mysql/grastate.dat"    
+            /usr/bin/echo "--jsmk-- THIS IS CLUSTER MASTER.... "   
+            /usr/bin/echo "--jsmk-- START galera_new_cluster IN glr `date` "   
+            /usr/bin/galera_new_cluster  
+        else
+            /usr/bin/echo "--jsmk-- CFGFILE safe_to_bootstrap: 0 IN /var/lib/mysql/grastate.dat"    
+            /usr/bin/echo "--jsmk-- WAIT FOR CLUSTER MASTER"    
+
+            tt=$[$RANDOM % 37  + $RANDOM % 57 + 37 + $( cat /var/log/messages | wc -l ) % 77  ] 
+            /usr/bin/echo "--jsmk-- START sleep and wakeup [ $tt sec ] IN glr `date` "   
+            /usr/bin/sleep $tt 
+            /usr/bin/echo "--jsmk-- START mariadb.service IN glr `date` BEGIN... " 
+
+            /usr/bin/systemctl start mariadb  
+
+            if [ $? != 0 ]; then
+                /usr/bin/echo "--jsmk-- START mariadb.service IN glr `date` [ -JUDGE- ] END..... " 
+            else
+                /usr/bin/echo "--jsmk-- START mariadb.service IN glr `date` END..... " 
+            fi
+
+        fi
+
         exit 0
 	;;
   stop)
-        ps -ef | grep loopglr.sh | grep -Ev 'grep' | awk '{print $2}'  | xargs kill -9
-        tt=$[$RANDOM % 37 + $RANDOM % 17 ] 
-        /usr/bin/echo " STOP sleep and wakeup IN glr $tt `date` "  ; 
+        pkill -9 loopglr.sh ;
+        #tt=$[$RANDOM % 37  + $RANDOM % 27 + $RANDOM % 17 + $RANDOM % 7 ] 
+        tt=$[$RANDOM % 57  + $RANDOM % 7 + $( cat /var/log/messages | wc -l ) % 37  ] 
+        /usr/bin/echo "--jsmk-- STOP sleep and wakeup [ $tt sec ] IN glr `date` "   
         /usr/bin/sleep $tt
-        /usr/bin/echo " STOP mariadb.service IN glr `date` " ;
+        /usr/bin/echo "--jsmk-- STOP mariadb.service IN glr `date` BEGIN... " 
         /usr/bin/systemctl stop mariadb   
+        /usr/bin/echo "--jsmk-- STOP mariadb.service IN glr `date` END..... " 
+        if [[ $( cat /var/lib/mysql/grastate.dat | grep 'safe_to_bootstrap: 1' ) = 'safe_to_bootstrap: 1' ]]; then  
+            /usr/bin/echo "--jsmk-- CFGFILE safe_to_bootstrap: 1 IN /var/lib/mysql/grastate.dat"   
+            /usr/bin/echo "--jsmk-- THIS IS CLUSTER MASTER.... "   
+        else
+            /usr/bin/echo "--jsmk-- CFGFILE safe_to_bootstrap: 0 IN /var/lib/mysql/grastate.dat"   
+            /usr/bin/echo "--jsmk-- THIS IS CLUSTER SLAVE..... "   
+        fi
+
         exit 0
 	;;
   status)
+        /usr/bin/echo "--jsmk-- STATUS mariadb.service IN glr `date` END..... " 
+        if [[ $( cat /var/lib/mysql/grastate.dat | grep 'safe_to_bootstrap: 1' ) = 'safe_to_bootstrap: 1' ]]; then  
+            /usr/bin/echo "--jsmk-- STATUS.CFGFILE safe_to_bootstrap: 1 IN /var/lib/mysql/grastate.dat"   
+            /usr/bin/echo "--jsmk-- STATUS.THIS IS CLUSTER MASTER.... "   
+        else
+            /usr/bin/echo "--jsmk-- STATUS.CFGFILE safe_to_bootstrap: 0 IN /var/lib/mysql/grastate.dat"   
+            /usr/bin/echo "--jsmk-- STATUS.THIS IS CLUSTER SLAVE..... "   
+        fi
+
 	/usr/bin/systemctl status mariadb -l && exit 0 || exit $?
 	  ;;
   restart|force-reload)
-        tt=$[$RANDOM % 77 + 17] 
-        /usr/bin/echo " RESTART sleep and wakeup IN glr $tt `date` "  ; 
+        #tt=$[$RANDOM % 37  + $RANDOM % 27 + $RANDOM % 17 + $RANDOM % 7 ] 
+        tt=$[$RANDOM % 57  + $RANDOM % 7 + $( cat /var/log/messages | wc -l ) % 37  ] 
+        /usr/bin/echo "--jsmk-- RESTART sleep and wakeup [ $tt sec ] IN glr `date` "   
         /usr/bin/sleep $tt
-        /usr/bin/echo " RESTART mariadb.service IN glr `date` " ;
+        /usr/bin/echo "--jsmk-- RESTART mariadb.service IN glr `date` BEGIN... " 
         /usr/bin/systemctl restart mariadb  
+        /usr/bin/echo "--jsmk-- RESTART mariadb.service IN glr `date` END..... " 
         exit 0
 	;;
   *)
